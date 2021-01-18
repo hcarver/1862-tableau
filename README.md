946e18aa57b8747b83bc01e71bc04b37ee9ddbdd
Hywel Carver <>
2021-01-18 21:01:51 +0000

    Build a 1862 tableau tool

--- README.md
+++ README.md
@@ -1,70 +1,27 @@
-# Getting Started with Create React App
+# No-shuffle 1862 tableau manager
+
+## What is this?
+
+I like the solo game in 1862, but if you don't shuffle the share certs thoroughly the game is easier. And shuffling 140 small cards is quite annoying, especially because playing the game (solo or multiplayer) means that card distribution is nothing like uniform.
+
+This project saves the shuffling. Tell it which companies you take out of the game, and how many certs you need to draw, and it'll tell you which certs to take.
+
+It runs in the browser, and it stores its state in your browser's local storage. That means that you can accidentally refresh the page, or close the window and come back to it, and the state of the share cards will be the same.
+
+It *doesn't* implement the actual tableau for you, yet.
+
+## Why does the code look like it was written in a hurry?
+
+It was written in one lunch break. I'll improve it as needed.
+
+## How can I help?
+
+### Improve it
+
+Install dependencies with `yarn install` then run the app with `yarn start`.
+
+Make your improvements, and send me a pull request. I'll be glad to see it. You might want to message me first and discuss the change. I'm `@h` on the 18xx Slack.
+
+### By the way
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
-
-In the project directory, you can run:
-
-### `yarn start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,186 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const COMPANIES = [
+  "WNR", "ENR", "W&F", "L&H", "L&D", "WStI", "L&E", "E&H", "NGC", "N&E",
+  "I&B", "N&B", "SVR", "ECR", "EUR", "ESR", "FDR", "WVR", "N&S", "Y&N"
+]
+
+class Tableau {
+  constructor() {
+    this.cards = new Map()
+    this.removed_companies = []
+
+    COMPANIES.forEach(company => {
+      this.cards.set(company, 7)
+    })
+  }
+
+  serialize() {
+    return JSON.stringify({
+      rc: this.removed_companies,
+      cards: JSON.stringify(Array.from(this.cards.entries()))
+    })
+  }
+
+  deserialize_from(str) {
+    if(str) {
+      const parsed = JSON.parse(str)
+
+      this.removed_companies = parsed['rc']
+      this.cards = new Map(JSON.parse(parsed['cards']))
+    }
+    return this;
+  }
+
+  current_count(company) {
+    return this.cards.get(company)
+  }
+
+  total_count() {
+    return Array.from(this.cards.values()).reduce((a, b) => a + b)
+  }
+
+  remove_company(company) {
+    const newTableau = new Tableau()
+    newTableau.removed_companies = [...this.removed_companies, company]
+    newTableau.cards = new Map(this.cards)
+    newTableau.cards.set(company, 0)
+    return newTableau
+  }
+
+  draw_card() {
+    const count = this.total_count()
+    if(count === 0) return [this, null];
+
+    const all_cards = [].concat(
+      ...COMPANIES.map((company) => Array(this.current_count(company)).fill(company))
+    )
+
+    const picked_card = all_cards[Math.floor(Math.random() * all_cards.length)]
+
+    const new_tableau = new Tableau()
+    new_tableau.removed_companies = [...this.removed_companies]
+    new_tableau.cards = new Map(this.cards)
+    new_tableau.cards.set(picked_card, this.cards.get(picked_card) - 1)
+
+    return [new_tableau, picked_card]
+  }
+}
 
 function App() {
+  const [drawnCards, setDrawnCards] = React.useState(JSON.parse(localStorage.getItem('drawn-cards') || '[]'));
+  const [tableau, setTableau] = React.useState(new Tableau().deserialize_from(localStorage.getItem('tableau')));
+  const inputRef = React.useRef()
+
+  React.useEffect( () => {
+    localStorage.setItem('tableau', tableau.serialize())
+  }, [tableau])
+
+  React.useEffect( () => {
+    localStorage.setItem('drawn-cards', JSON.stringify(drawnCards))
+  }, [drawnCards])
+
+  const drawCardButton = () => {
+    const count = parseInt(inputRef.current.value) || 1
+
+    const new_cards = []
+    let last_tableau = null;
+    for(let i = 0; i < count; ++i) {
+      const [new_tableau, card] = tableau.draw_card()
+      if(card) {
+        new_cards.push(card)
+        last_tableau = new_tableau
+      }
+    }
+    setDrawnCards([...new_cards, ...drawnCards])
+
+    if(last_tableau) {
+      setTableau(last_tableau)
+    }
+
+    inputRef.current.value = 1
+  }
+
+  const removed_companies = tableau.removed_companies
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          1862 tableau builder
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <small>
+          Play 1862 solo without all the damned shuffling.
+        </small>
       </header>
+      <div className="card-columns">
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Deck state</h5>
+            <p className="card-text">
+              <p>
+                {`Cards in deck:
+                  ${tableau.total_count()}`}
+              </p>
+              <p>
+                Card counts:
+                {" "}
+                {COMPANIES.map(company => `${company}: ${tableau.current_count(company)}`).join(", ")}
+              </p>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Draw cards</h5>
+            <p className="card-text">
+              <p>
+                <div className="form-inline justify-content-center">
+                  <input className="form-control" type="number" min="1" ref={inputRef} placeholder="How many to draw"/>
+                  <button className="btn btn-primary" onClick={drawCardButton}>Draw card</button>
+                </div>
+              </p>
+              <p>
+                Cards you've drawn (most recent first)
+              </p>
+              <ul className="list-unstyled">
+                {drawnCards.map(c => <li>{c}</li>)}
+              </ul>
+            </p>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <h5 className="card-title">Companies</h5>
+            <p className="card-text">
+              Removed companies:
+            </p>
+            <ul className="list-unstyled">
+              {removed_companies.map(company => <li>{company}</li>)}
+            </ul>
+            <p>
+              Remaining companies:
+            </p>
+            <ul className="list-unstyled">
+              {
+                COMPANIES.filter(c => !tableau.removed_companies.includes(c)).map(
+                  company => <li>
+                    <button className="btn btn-link py-0" aria-label={`Remove ${company}`} onClick={(e) => {setTableau(tableau.remove_company(company));}}>
+                      <span aria-hidden="true" className="text-danger">&times;</span>
+                      {" "}
+                      {company}
+                    </button>
+                  </li>
+                )
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button className="btn btn-block btn-warning" onClick={() => {setTableau(new Tableau()); setDrawnCards([]);}}>RESET</button>
+      </div>
     </div>
   );
 }
